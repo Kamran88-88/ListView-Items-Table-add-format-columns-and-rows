b65b26e85d0ae656054db62cfefa57f37c3b70f9
Kamran Aliyev <kamran88-88@outlook.com>
2018-07-25 22:10:45 +0400

    Add project files.

---
+++ WindowsFormsApp14/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp14
+{
+    public partial class Form1 : Form
+    {
+        User user = new User();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Type us1 = user.GetType();
+            listView1.Columns.Add("ID");
+            foreach (var item in us1.GetProperties())
+            {
+                listView1.Columns.Add(item.Name);
+               // listView1.Items.Add(item.GetValue(item));
+            }
+            
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+
+            var list = new ListViewItem();
+
+            ListViewItem viewItem = new ListViewItem("1");
+            foreach (var item in Form2.user.GetType().GetProperties())
+            {
+                viewItem.SubItems.Add(item.GetValue(Form2.user).ToString());
+            }
+
+            listView1.Items.Add(viewItem);
+        }
+    }
+}
---
+++ WindowsFormsApp14/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp14
+{
+    public partial class Form2 : Form
+    {
+      public static User user = new User();
+        
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+           
+            user.Name = textBox1.Text;
+            user.Surname = textBox2.Text;
+            user.Age =textBox3.Text;
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            Close();
+        }
+    }
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Age { get; set; }
+    }
+
+}
